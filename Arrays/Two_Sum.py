d2f04e98a0013a043df36a635dfb390b4fc6b627
Harsha Valaboju <harshavardhanuvalaboju91@gmail.com>
2026-07-01 23:52:32 +0530

    Solve Rotate Array

---
+++ Arrays/Two_Sum.py
@@ -0,0 +1,24 @@
+"""
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+"""
+
+class Solution:
+    def rotate(nums, k):
+        n=len(nums)
+        k%=n
+        def reverse(left,right):
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+
+    k=int(input())
+    nums=list(map(int,input().split()))
+    print(rotate(nums,k))
+
+        
